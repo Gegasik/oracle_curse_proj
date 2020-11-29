962cf15077398ddefab6f725a64a87b970d550c1
DESKTOP-OM6GAL2\влен 4.0 <vladreut2000@mail.ru>
2020-11-30 00:57:56 +0300

    add almost all create drop scripts to entities

---
+++ Services/SessionService.sql
@@ -0,0 +1,22 @@
+create or replace procedure CreateSession(filmId integer, cinemaHallId integer,addedCoast integer,"date" date)
+is
+begin
+insert into "SESSION" s(s.film_id,s.cinema_hall_id,s.added_coast,s."date") 
+              values(filmId,cinemaHallId,addedCoast,"date");
+  commit;
+end;
+--****************************************************************************************************
+create or replace procedure DeleteSessionById (SessionId in number)
+is 
+begin
+  delete from "SESSION" s where s.id = SessionId;
+  commit;
+end;
+--****************************************************************************************************
+create or replace procedure UpdateSession (filmId integer, cinemaHallId integer,addedCoast integer,"date" date,id integer)
+is 
+begin
+  update "SESSION" s set s.film_id = filmId,s.cinema_hall_id=cinemaHallId,s.added_coast=addedCoast,s."date"="date"
+  where s.id=id ;
+  commit;
+end;
---
+++ Services/dropProcedure.sql
@@ -0,0 +1,18 @@
+drop procedure CREATECATEGORY;
+drop procedure DeleteCategoryById;
+drop procedure DeleteCategoryByTitle;
+drop procedure UpdateCategory;
+--****************************************************************************************************
+drop procedure CreateCinemaHall;
+drop procedure DeleteCinemaHallById;
+drop procedure DeleteCinemaHallByTitle;
+drop procedure UpdateCinemaHall;
+******************************************************************************************************
+drop procedure CreateFilm;
+drop procedure DeleteFilmById;
+drop procedure DeleteFilmByName;
+drop procedure UpdateFilm;
+******************************************************************************************************
+drop procedure addCategoryToFilm;
+drop procedure deleteCategoryFromFilm;
+******************************************************************************************************
